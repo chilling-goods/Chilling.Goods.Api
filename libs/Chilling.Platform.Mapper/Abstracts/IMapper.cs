e7d0af8af118b2447b1c8574536da32548bef579
Ebahhh <grom41098@gmail.com>
2022-08-26 14:45:20 +0300

    Добавление простого маппера в проект (#11)
    Co-authored-by: Roman <Roman@DESKTOP-JSQ3L9G>

---
+++ libs/Chilling.Platform.Mapper/Abstracts/IMapper.cs
@@ -0,0 +1,9 @@
+﻿namespace Chilling.Platform.Mapper.Abstracts
+{
+    public interface IMapper
+    {
+        TResult Map<TIn, TResult>(TIn model);
+
+        IEnumerable<TResult> Map<TIn, TResult>(IEnumerable<TIn> model);
+    }
+}
